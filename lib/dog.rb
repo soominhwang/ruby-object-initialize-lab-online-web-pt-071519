d84bc9df8cb21bd049298665daef5580824acafa
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-18 20:06:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(name, breed="Mutt")
+      @name = name
+      @breed = breed
+    end
+
+    def name=(name, breed)
+      @name = name
+      @breed = breed
+    end
+
+    def name
+      @name
+    end
+end
